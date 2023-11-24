94c99c8d61429f14d563e28e3e0fc1da1b0dc5e4
JordyDelPezo <jordy.delpezo@ug.edu.ec>
2023-11-23 20:44:26 -0500

    Examen

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.page.scss'],
 })
 export class FormularioPage {
-  nombre: string= '';
-  email: string= '';
-  telefono: string= '';
-  fechaNacimiento: string= '';
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
+  fechaNacimiento: string = '';
   edad: number = 0;
-
 
   mostrarDatos() {
     if (this.validarFormulario()) {
@@ -19,38 +18,54 @@ export class FormularioPage {
       console.log('Correo Electrónico:', this.email);
       console.log('Teléfono:', this.telefono);
       console.log('Fecha de Nacimiento:', this.fechaNacimiento);
+
+      // Llamar a la función calcularEdad() y mostrar la edad calculada
+      this.calcularEdad();
     } else {
       console.log('Por favor, complete todos los campos del formulario.');
     }
   }
 
   validarFormulario(): boolean {
-    return !!this.nombre && !!this.email && !!this.telefono && !!this.fechaNacimiento;
+    return (
+      !!this.nombre &&
+      !!this.email &&
+      !!this.telefono &&
+      !!this.fechaNacimiento &&
+      this.validarNumero()
+    );
   }
-  validarNumero() {
+
+  validarNumero(): boolean {
     // Eliminar caracteres no numéricos
     let numeroLimpio = this.telefono.replace(/\D/g, '');
 
-    // Verificar si hay exactamente 10 números
-    if (numeroLimpio.length === 10) {
+    // Verificar si hay exactamente 10 números y si todos los caracteres son números
+    if (numeroLimpio.length === 10 && /^\d+$/.test(numeroLimpio)) {
       console.log('Número válido:', numeroLimpio);
+      return true;
     } else {
-      console.log('Número inválido. Debe contener 10 números.');
+      console.log('Número inválido. Debe contener 10 números y solo caracteres numéricos.');
+      return false;
     }
   }
+
   calcularEdad() {
     const fechaNacimientoDate = new Date(this.fechaNacimiento);
     const fechaActual = new Date();
 
     const diferenciaAnios = fechaActual.getFullYear() - fechaNacimientoDate.getFullYear();
 
-    if (fechaNacimientoDate.getMonth() > fechaActual.getMonth() ||
-        (fechaNacimientoDate.getMonth() === fechaActual.getMonth() &&
-         fechaNacimientoDate.getDate() > fechaActual.getDate())) {
-      const edad = diferenciaAnios - 1;
-      console.log('Edad calculada:', edad);
+    if (
+      fechaNacimientoDate.getMonth() > fechaActual.getMonth() ||
+      (fechaNacimientoDate.getMonth() === fechaActual.getMonth() &&
+        fechaNacimientoDate.getDate() > fechaActual.getDate())
+    ) {
+      this.edad = diferenciaAnios - 1;
+      console.log('Edad calculada:', this.edad);
     } else {
-      console.log('Edad calculada:', diferenciaAnios);
+      this.edad = diferenciaAnios;
+      console.log('Edad calculada:', this.edad);
     }
   }
 }
